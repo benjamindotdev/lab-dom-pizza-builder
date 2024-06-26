a5b2ab1e02c12511b4dfb47fde8b5af4dcec7ca7
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-06-26 16:21:20 +0200

    updating with AI suggestions

--- js/index.js
+++ js/index.js
@@ -87,12 +87,16 @@ function renderGlutenFreeCrust() {
 }
 
 function renderButtons() {
-  // Iteration 3: add/remove the class "active" of each `<button class="btn">`
-  if (state.pepperoni) {
-    document.querySelector('.btn.btn-pepperoni').classList.add('active');
-  } else {
-    document.querySelector('.btn.btn-pepperoni').classList.remove('active');
-  }
+  const buttons = document.querySelectorAll('.btn');
+
+  buttons.forEach((button) => {
+    const ingredient = button.classList[1].split('-')[1];
+    if (state[ingredient]) {
+      button.classList.add('active');
+    } else {
+      button.classList.remove('active');
+    }
+  });
 }
 
 function renderPrice() {
